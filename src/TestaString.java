55453163c692b60a11e216ca3eaf386ea684245e
Luiz Felipe <40125808+luizfelipedasilva678@users.noreply.github.com>
2021-11-27 17:22:45 -0300

    Add files via upload

---
+++ src/TestaString.java
@@ -0,0 +1,8 @@
+public class TestaString {
+	public static void main(String[] args) {
+		char letra = 'A';
+		int codLetra = letra;
+		letra = (char) (letra + 2);
+		String nome = "Testando";
+	}
+}
