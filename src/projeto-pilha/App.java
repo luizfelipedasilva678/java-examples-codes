d41f6a98ada7e44c2bcba907fc3ac5b953bed1c0
Luiz Felipe <luizfelipedasilva678@gmail.com>
2022-02-02 21:09:16 -0300

    Exception

--- src/projeto-pilha/App.java
+++ src/projeto-pilha/App.java
@@ -13,6 +13,11 @@ public class App {
 
     private static void metodo2() {
         System.out.println("Inicio metodo2");
+        try {
+            int x = 10/0;
+        } catch (Exception e) {
+            System.out.println("Error " +  e.getMessage());
+        }
         int[] array =  new int[5];
         for(int i = 0; i < 5; i++) {
             array[i] = i;
