5c79c238bebb42efcc958ecc809be28b127e9210
Luiz Felipe <luizfelipedasilva678@gmail.com>
2022-02-18 19:40:51 -0300

    JavaLang

---
+++ src/javalang/TestaJavaLang.java
@@ -0,0 +1,9 @@
+public class TestaJavaLang {
+    public static void main(String[] args) {
+        int x = 5;
+        String nome = "CEFET";
+        StringBuilder nome2 = new StringBuilder("Cefet");
+
+        System.out.println(nome.toLowerCase());
+    }   
+}
